a212a1c6b51167cd495176a141984d28c318fbbc
Tamas Sallai <tamas.sallai@advancedweb.hu>
2022-12-08 11:05:58 +0100

    Fix

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-CA=$(terraform output -raw ca) IOT_ENDPOINT=$(terraform output -raw iot_endpoint) CERT=$(terraform output -raw cert) KEY=$(terraform output -raw key) THING_NAME=$(terraform output -raw thing_name) node index.js
+npm ci && CA=$(terraform output -raw ca) IOT_ENDPOINT=$(terraform output -raw iot_endpoint) CERT=$(terraform output -raw cert) KEY=$(terraform output -raw key) THING_NAME=$(terraform output -raw thing_name) node index.js
 
